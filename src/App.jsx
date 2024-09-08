61887f9e2e3f2ef9ef1052c337dcf3651fc28a53
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-09-08 17:40:11 +0530

    Add Header component including SVG logo and title, styled with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-export default function App() {
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    <Header />
+    </div>
   )
 }
+
+export default App
 
