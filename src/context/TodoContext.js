b7eb96cb8d7b4cf453fd23cb791c0ed43dd6874a
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-09-08 17:31:16 +0530

    Create Vite project with React template and Tailwind CSS setup

---
+++ src/context/TodoContext.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            title: "Todo msg",
+            description: "none",
+            date: "none",
+            status: "none",
+            priority: "none",
+            completed: false,
+        },
+    ],
+    addTodo: (todo) => { },
+    updateTodo: (todo, id) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { },
+});
+
+export const TodoProvider = TodoContext.Provider;
+
+export const useTodo = () => {
+    return useContext(TodoContext);
+};
